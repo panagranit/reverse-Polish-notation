2e5f298c3174c5ff72a774c152211dd4bdac7b3d
cliff hoiwell <panagranit@gmail.com>
2022-01-25 17:42:43 +0000

    made it less wordy

--- main.py
+++ main.py
@@ -1,43 +1,38 @@
 
 '''The expression is given as a list of numbers and operands. For example: [5, 3, '+'] should return 5 + 3 = 8.
 
-For example, [15, 7, 1, 1, '+', '-', '/', 3, '*', 2, 1, 1, '+', '+', '-'] should return 5, since it is equivalent to ((15 / (7 - (1 + 1))) * 3) - (2 + (1 + 1)) = 5.'''
+For example, [15, 7, 1, 1, '+', '-', '/', 3, '*', 2, 1, 1, '+', '+', '-'] should return 5, since it is equivalent to ((15 / (7 - (1 + 1))) * 3) - (2 + (1 + 1)) = 5.
+
+ This like reverse polish notation in calculator only the stack can be Infinite!  '''
 
 
-languages = ['Python', 'C', 'C++', 'C#', 'Java']
 
-#Bad way
+stack = [15, 7, 1, 1, '+', '-', '/', 3, '*', 2, 1, 1, '+', '+', '-']
 
-#Good Way
-
-
+#counts_of_operators
+def operatorcount(stack):
+  
+  for i, x in enumerate(stack):
+    x = str(x)
+    try:
+     eval(x)# eval works because it returns error if an operator is found
+    except:
+     
+     i += 1
+  return (i)
+lenofop = operatorcount(stack)
 #this does binary operation between the stack elements
 
 def revb(x,y,z):
   
   res = str(x)+str(z)+str(y)
   return eval(res)
-# x + y
 
-
-
-# This will determine if the string is a function or a number.
-
-
- 
 
 stack = [15, 7, 1, 1, '+', '-', '/', 3, '*', 2, 1, 1, '+', '+', '-']
 print(stack) 
-# append() function to push
-# element in the stack
 
-#stack.append('a')
-
-#stack.append('b')
-
-#stack.append('c')
-
-for w in range(10):
+for w in range(lenofop):
   for i, x in enumerate(stack):
     x = str(x)
     try:
@@ -48,7 +43,7 @@ for w in range(10):
      print(' operator found', x)
      print( "position",i)
      #print(stack)
-   # Pop the first two items from the stack and then call my function to perform the mathematical
+   # Pop the first three items from the stack and then call my function to perform the mathematical
    # operation then put that result back into the stack then go to the next item in the list
      z =stack.pop(i)
      y = stack.pop(i-1)
